bcda9f5044900edef216f65c4161f7cba2b6866a
KKaiyrbekov <han_umag@mail.com>
2023-01-13 20:56:46 +0600

    first commit

---
+++ src/test/java/HomeWork.java
@@ -0,0 +1,103 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class HomeWork {
+
+    @Test
+    void singleUserTest() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.first_name", is("Janet"),
+                        "data.last_name", is("Weaver"));
+    }
+
+    @Test
+    void createTest() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name",is("morpheus"))
+                .body("job",is("leader"));
+    }
+
+    @Test
+    void updateTest() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void deletedTest() {
+        given()
+                .log().uri()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+    }
+
+    @Test
+    void loginUnsuccessful() {
+        String data = "{ \"email\": \"peter@klaven\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error",is("Missing password"));
+    }
+
+    @Test
+    void registerUnsuccessful() {
+        String data = "{ \"email\": \"sydney@fife\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error",is("Missing password"));
+    }
+}
